a07eedff5533be1bb8b92b3cc385bfab7ab71c41
girishbabu.manjunath <girishbabu.manjunath@servicenow.com>
2020-07-17 16:19:09 +0530

    Updates

---
+++ precache-manifest.7cb19c2db3c890f8e411fb11546b4bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5f09f837bf2d256b91dd5cd4fdcd7c",
+    "url": "/monsters-collection/index.html"
+  },
+  {
+    "revision": "da4a478be9f378fecee9",
+    "url": "/monsters-collection/static/css/main.965c1fb1.chunk.css"
+  },
+  {
+    "revision": "f7774d736550077f9030",
+    "url": "/monsters-collection/static/js/2.86031bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-collection/static/js/2.86031bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4a478be9f378fecee9",
+    "url": "/monsters-collection/static/js/main.f32a13c5.chunk.js"
+  },
+  {
+    "revision": "49a783dcd760140e74d7",
+    "url": "/monsters-collection/static/js/runtime-main.badee0de.js"
+  }
+]);
